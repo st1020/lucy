8e09564398b5d53f4c99ccda61d73b3fb280fb9d
st1020 <stone_1020@qq.com>
2022-05-22 00:19:42 +0800

    feat: 修改 stdio 标准库名称为 io

--- lucy/libs/__init__.py
+++ lucy/libs/__init__.py
@@ -1,11 +1,11 @@
 from ..lucy_data import TableData
 
 from .convert import lib_table as convert_lib_table
-from .stdio import lib_table as stdio_lib_table
+from .io import lib_table as io_lib_table
 from .table import lib_table as table_lib_table
 
 lib_table = TableData({
     'convert': convert_lib_table,
-    'stdio': stdio_lib_table,
+    'io': io_lib_table,
     'table': table_lib_table,
 })
